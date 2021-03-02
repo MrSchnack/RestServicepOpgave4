b118a64045b4ee687eb1f2be815e9c50f304faf8
MrSchnack <philip.s.petersen@gmail.com>
2021-03-02 18:43:06 +0100

    Add project files.

---
+++ Opgave4/Controllers/BeerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Opgave1;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Opgave4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private static BeerManager _manager = new BeerManager();
+
+        // GET: api/<BeerController>
+        [HttpGet]
+        public IEnumerable<Beer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BeerController>/5
+        [HttpGet("{id}")]
+        public Beer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<BeerController>
+        [HttpPost]
+        public Beer Post([FromBody] Beer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BeerController>/5
+        [HttpPut("{id}")]
+        public Beer Put(int id, [FromBody] Beer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<BeerController>/5
+        [HttpDelete("{id}")]
+        public Beer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Opgave4/Managers/BeerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Opgave1;
+
+namespace Opgave4.Controllers
+{
+    public class BeerManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Beer> Data = new List<Beer>
+        {
+            new Beer {Id = _nextId++, Name = "Tuborg", Price = 12.50, Abv = 4.6 },
+            new Beer {Id = _nextId++, Name = "Carlsberg", Price = 13.50, Abv = 4.5},
+            new Beer {Id = _nextId++, Name = "Guiness", Price = 14.50, Abv = 5.5}
+        };
+
+        public BeerManager()
+        {
+        }
+        
+        public List<Beer> GetAll()
+        {
+            return new List<Beer>(Data);
+        }
+
+        public Beer GetById(int id)
+        {
+            return Data.Find(beer => beer.Id == id);
+        }
+
+        public Beer Add(Beer newBeer)
+        {
+            newBeer.Id = _nextId++;
+            Data.Add(newBeer);
+            return newBeer;
+        }
+        public Beer Delete(int id)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            Data.Remove(beer);
+            return beer;
+        }
+        public Beer Update(int id, Beer updates)
+        {
+            Beer beer = Data.Find(beer1 => beer1.Id == id);
+            if (beer == null) return null;
+            beer.Name = updates.Name;
+            beer.Price = updates.Price;
+            beer.Abv = updates.Abv;
+            return beer;
+
+
+        }
+
+    };
+}
